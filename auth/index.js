3eb57b84183c526b207bd50a00471576cc3f498f
Waqqars987 <Waqqars987@gmail.com>
2020-02-12 15:15:19 +0530

    new auth

---
+++ auth/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const authRoute = require('./routes/auth');
+
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => {
+    console.log(`Node server is running...`);
+});
