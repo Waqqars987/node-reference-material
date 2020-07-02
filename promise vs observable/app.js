12b141b057204ee231ae166870ad04d6b585ace6
Waqqars987 <Waqqars987@gmail.com>
2020-07-02 12:54:07 +0530

    changes

--- promise vs observable/app.js
+++ promise vs observable/app.js
@@ -1,34 +1,39 @@
 const { Observable } = require('rxjs');
 const { takeWhile } = require('rxjs/operators');
+console.clear();
 
-const promiseBehavior = () => {
+/* The following example shows:
+promises are eager and can handle only single value while
+observables are lazy and can handle a stream of values*/
+
+const promiseBehavior = new Promise(resolve => {
 	let value = 0;
-	return new Promise(resolve => {
-		const promiseInterval = setInterval(() => {
-			value++;
-			if (value <= 5) {
-				resolve(value);
-			} else {
-				clearInterval(promiseInterval);
-			}
-		}, 1000);
-	});
-};
+	console.log(`2. Promise:: Executing`);
+	const promiseInterval = setInterval(() => {
+		value++;
+		if (value <= 5) {
+			resolve(value);
+		} else {
+			clearInterval(promiseInterval);
+		}
+	}, 1000);
+});
 
-promiseBehavior().then(response => console.log(`Promise Log: ${response}`));
+console.log(`1. Promise:: Subscribing`);
+promiseBehavior.then(response => console.log(`3. Promise:: Result: ${response}`));
 
-const observableBehavior = () => {
+const observableBehavior = new Observable(observer => {
 	let value = 0;
-	return new Observable(observer => {
-		const observableInterval = setInterval(() => {
-			value++;
-			if (value <= 5) {
-				observer.next(value);
-			} else {
-				clearInterval(observableInterval);
-			}
-		}, 1000);
-	});
-};
+	console.log(`2. Observable:: Executing`);
+	const observableInterval = setInterval(() => {
+		value++;
+		if (value <= 5) {
+			observer.next(value);
+		} else {
+			clearInterval(observableInterval);
+		}
+	}, 1000);
+});
 
-observableBehavior().pipe(takeWhile(val => val <= 5)).subscribe(data => console.log(`Observable Log: ${data}`));
+console.log(`1. Observable:: Subscribing`);
+observableBehavior.pipe(takeWhile(val => val <= 5)).subscribe(data => console.log(`3. Observable:: Result: ${data}`));
