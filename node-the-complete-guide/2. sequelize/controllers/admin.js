4fb5ebffd465b75f5c57dcfe2f8ec6bc4bf8cb36
Waqqar Suleman <waqqars987@gmail.com>
2021-01-26 16:26:00 +0530

    completed sequelize section

--- node-the-complete-guide/2. sequelize/controllers/admin.js
+++ node-the-complete-guide/2. sequelize/controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	Product.create({
-		title,
-		price,
-		imageUrl,
-		description,
-	})
+	req.user
+		.createProduct({
+			title,
+			price,
+			imageUrl,
+			description,
+		})
 		.then(result => {
 			// console.log(result);
 			console.log('Created Product');
@@ -33,8 +34,15 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect('/');
 	}
 	const prodId = req.params.productId;
-	Product.findByPk(prodId)
-		.then(product => {
+	req.user
+		.getProducts({
+			where: {
+				id: prodId,
+			},
+		})
+		// Product.findByPk(prodId)
+		.then(products => {
+			const product = products[0];
 			if (!product) {
 				return res.redirect('/');
 			}
@@ -70,7 +78,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-	Product.findAll()
+	req.user
+		.getProducts()
+		// Product.findAll()
 		.then(products => {
 			res.render('admin/products', {
 				prods: products,
